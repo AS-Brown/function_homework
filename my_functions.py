0fa9deb7cbbced6fae737f7e1ceb941c68dc607e
AS-Brown <andrewsbrown@hotmail.co.uk>
2021-08-31 20:10:07 +0100

    first commit

---
+++ my_functions.py
@@ -0,0 +1,74 @@
+# def greet():
+#     return "Hey"
+
+# greeting = greet()
+# print(greeting)
+
+# def greet():
+#     print("Hey")
+# greeting = greet()
+# print(greeting)
+
+
+# def greet(name):
+#   return "Hey " + name
+
+# greeting = greet("Bob")
+# print(greeting)
+
+# def greet(name, time_of_day):
+#     return ("Good " + time_of_day + ", " + name)
+
+# greeting = greet("Bob", "morning")
+# print(greeting)
+
+# def last():
+#     print(greeting)
+
+# def greet(name, time_of_day):
+#     return("Good " + time_of_day + ", " + name)
+
+# name_1 = "Bob"
+# time_of_day_1 = "morning"
+# greeting = greet(name_1, time_of_day_1)
+# last()
+
+# name_2 = "Ada"
+# time_of_day_2 = "afternoon"
+# greeting = greet(name_2, time_of_day_2)
+# last()
+
+# def greet():
+#     words = "hey"
+#     return words
+
+# def greet2():
+#     return words
+# print(greet2())
+
+chickens = [
+    {"name": "Margaret", "age": 2, "eggs": 0},
+    {"name": "Hetty", "age": 1, "eggs": 2},
+    {"name": "Henrietta", "age": 3, "eggs": 1},
+    {"name": "Audrey", "Age": 2, "eggs": 0},
+    {"name": "Mabel", "age": 5, "eggs": 1}
+]
+
+# total_eggs = 0
+
+# for chicken in chickens:
+#     total_eggs += chicken["eggs"]
+#     chicken["eggs"] = 0 
+
+# print(f"{total_eggs} eggs collected")
+
+def count_eggs(list):
+    total_eggs = 0
+
+    for bird in list:
+        total_eggs += bird["eggs"]
+        bird["eggs"] = 0
+    
+    return f"{total_eggs} eggs collected"
+
+print(count_eggs(chickens))
